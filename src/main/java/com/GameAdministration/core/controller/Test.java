43fa25770a7a515789416433abcb5e9d7f1bd5a4
lyp <lyp15561081477@dingtalk.com>
2021-01-22 15:30:37 +0800

    基本框架构成

--- src/main/java/com/GameAdministration/core/controller/Test.java
+++ src/main/java/com/GameAdministration/core/controller/Test.java
@@ -1,11 +1,22 @@
 package com.GameAdministration.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "test")
 public class Test {
+	
+	@Autowired
+	@Qualifier(value = "userJdbcTemplate")
+	JdbcTemplate userJdbcTemplate;
+	
+	@Autowired
+	@Qualifier(value = "gameJdbcTemplate")
+	JdbcTemplate gameJdbcTemplate;
 	
 	@RequestMapping(value = "hello")
 	public String hello(){
@@ -14,7 +25,14 @@ public class Test {
 	
 	@RequestMapping(value = "love")
 	public String love(){
-		return "我爱你";
+		String sql = "select username from userinfo where uid = 10001";
+		return userJdbcTemplate.queryForObject(sql, String.class);
+	}
+	
+	@RequestMapping(value = "love2")
+	public String love2(){
+		String sql = "select name from game where uid = 1";
+		return gameJdbcTemplate.queryForObject(sql, String.class);
 	}
 
 }
